a66fc302820fbdebab5bdcfd1fe89ee76c2e4760
Santosh Bachar <santoshbachar@gmail.com>
2024-02-24 21:51:35 +0530

    Fix agent-transition chart

--- assets/js/pages/agent/agent-state-transition-chart.js
+++ assets/js/pages/agent/agent-state-transition-chart.js
@@ -23,15 +23,18 @@ import Agents from "../../charts/agents";
 import $ from "jquery";
 import Gantt from "../../lib/chart-utils/gantt";
 import moment from "../../lib/moment-humanize-for-gocd";
+import SimpleStackedBar from "../../santosh/simple-stacked-bar";
+import drawChartStats from "./header";
+import showLegend from "./footer";
 
 let transportRef, agentUUID;
 const DATE_FMT = "YYYY-MM-DD";
 const TODAY = moment().format(DATE_FMT);
 
 const range = [
-  {id: "30", text: "Last 30 Days", start: moment(TODAY).subtract(30, "days").format(DATE_FMT), selected: true},
-  {id: "7", text: "Last 7 Days", start: moment(TODAY).subtract(7, "days").format(DATE_FMT), selected: false},
-  {id: "1", text: "Last 24 Hours", start: moment(TODAY).subtract(1, "days").format(DATE_FMT), selected: false}
+    {id: "30", text: "Last 30 Days", start: moment(TODAY).subtract(30, "days").format(DATE_FMT), selected: true},
+    {id: "7", text: "Last 7 Days", start: moment(TODAY).subtract(7, "days").format(DATE_FMT), selected: false},
+    {id: "1", text: "Last 24 Hours", start: moment(TODAY).subtract(1, "days").format(DATE_FMT), selected: false}
 ];
 
 // For whatever reason, using _.find() seems to break unrelated charts...???
@@ -39,57 +42,88 @@ const range = [
 // to disappear from bar charts. Really don't know, and spent too much time
 // looking into it.
 function byId(id) {
-  for (let i = 0, len = range.length; i < len; ++i) {
-    if (range[i].id === id) return range[i];
-  }
-  return null;
+    for (let i = 0, len = range.length; i < len; ++i) {
+        if (range[i].id === id) return range[i];
+    }
+    return null;
 }
 
 function renderGanttChart(initialData, transport) {
-  const parsedData = JSON.parse(initialData);
-  transportRef = transport;
-  agentUUID = parsedData.uuid;
-  const transitions = parsedData.transitions;
-  const config = Agents.transitions("chart-container", transitions, range, redraw);
+    const parsedData = JSON.parse(initialData);
 
-  Gantt.draw(config);
+    console.log('parsedData = ', parsedData);
+
+    transportRef = transport;
+    agentUUID = parsedData.uuid;
+    const transitions = parsedData.transitions;
+    const config = Agents.transitions("chart-container", transitions, range, redraw);
+
+    console.log('config = ', config);
+
+    // Gantt.draw(config);
+
+    // document.getElementById("chart-container").style.height = "350px";
+
+    drawChartStats(range, fetchAndRedraw);
+    new SimpleStackedBar("Agent State Transitions", parsedData);
+    showLegend();
 }
 
 function setSelection(days) {
-  range.forEach((r) => {
-    r.selected = (days === r.id);
-  });
+    range.forEach((r) => {
+        r.selected = (days === r.id);
+    });
 }
 
 function showSpinner() {
-  $(".spinner-overlay").removeClass("hidden");
+    $(".spinner-overlay").removeClass("hidden");
 }
 
 function hideSpinner() {
-  $(".spinner-overlay").addClass("hidden");
+    $(".spinner-overlay").addClass("hidden");
 }
 
 function redraw(days) {
-  days = days.toString();
-  setSelection(days);
+    days = days.toString();
+    setSelection(days);
 
-  const params = {
-    agent_uuid: agentUUID,
-    start: byId(days).start,
-    end: TODAY,
-    type: "agent",
-    metric: "agent_state_transition"
-  };
+    const params = {
+        agent_uuid: agentUUID,
+        start: byId(days).start,
+        end: TODAY,
+        type: "agent",
+        metric: "agent_state_transition"
+    };
 
-  showSpinner();
-  transportRef.request("fetch-analytics", params).done((data) => {
-    renderGanttChart(data, transportRef);
-    hideSpinner();
-  }).fail(console.error); //eslint-disable-line no-console
+    showSpinner();
+
+    console.log('in redraw() about to request from fetch-analytics, params = ', params);
+
+    transportRef.request("fetch-analytics", params).done((data) => {
+        renderGanttChart(data, transportRef);
+        hideSpinner();
+    }).fail(console.error); //eslint-disable-line no-console
+}
+
+function fetchAndRedraw(days) {
+    const params = {
+        agent_uuid: agentUUID,
+        start: byId(days).start,
+        end: TODAY,
+        type: "agent",
+        metric: "agent_state_transition"
+    };
+
+    console.log('fetching latest data for', params);
+
+    transportRef.request("fetch-analytics", params).done((data) => {
+        renderGanttChart(data, transportRef);
+    }).fail(console.error);
+
 }
 
 AnalyticsEndpoint.onInit(function (initialData, transport) {
-  renderGanttChart(initialData, transport);
+    renderGanttChart(initialData, transport);
 });
 
 AnalyticsEndpoint.ensure("v1");
--- assets/js/pages/analytics-tab/global/agent-with-highest-utilization-chart.js
+++ assets/js/pages/analytics-tab/global/agent-with-highest-utilization-chart.js
@@ -17,25 +17,38 @@
 import "css/global";
 
 import AnalyticsEndpoint from "gocd-server-comms";
-import H from "js/lib/load-highcharts.js";
 import DrilldownSupport from "js/lib/drilldown-support.js";
 import AgentChartFactories from "js/factories/agent-chart-factories.js";
 import JobChartFactories from "js/factories/job-chart-factories.js";
+import H from "js/lib/load-highcharts.js";
+import StackedBar from "../../../santosh/stacked-bar";
+import GraphManager from "../../../santosh/GraphManager";
 
 AnalyticsEndpoint.onInit(function (initialData, transport) {
-  const agents     = JSON.parse(initialData),
-        factory    = AgentChartFactories.get("AgentsMostUtilized"),
+    const agents = JSON.parse(initialData),
+        factory = AgentChartFactories.get("AgentsMostUtilized"),
         drilldowns = ["LongestWaitingJobsOnAgent", "JobBuildTimeOnAgent"],
-        container  = document.getElementById("chart-container");
+        container = document.getElementById("chart-container");
 
-  const config = DrilldownSupport.withDrilldown(
-    factory.config(agents),
-    drilldowns,
-    JobChartFactories,
-    transport
-  );
+    const config = DrilldownSupport.withDrilldown(
+        factory.config(agents),
+        drilldowns,
+        JobChartFactories,
+        transport
+    );
 
-  H.chart(container, config);
+    // console.log("agents = ", agents);
+    // H.chart(container, config);
+
+    // const name = ['x', ...agents.map(a => a.agent_host_name)];
+    // const data1 = ['Idle Duration Secs', ...agents.map(a => a.idle_duration_secs)];
+    // new StackedBar(transport, drilldowns, name, data1, undefined, agents);
+
+    // new StackedBar("AgentsMostUtilized", agents, drilldowns, transport);
+
+    const graphManager = new GraphManager('series', transport);
+    graphManager.initSeries('AgentsMostUtilized', agents);
+
 });
 
 AnalyticsEndpoint.ensure("v1");
