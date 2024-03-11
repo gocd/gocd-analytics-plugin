39cf30693bc38129f5924426fa1f6bbf073e308c
Santosh Bachar <santoshbachar@gmail.com>
2024-03-11 16:55:16 +0530

    1. Remove comments 2. Reorder non-init functions 3. Format code

--- assets/js/pages/analytics-tab/global/stage-timeline-chart.js
+++ assets/js/pages/analytics-tab/global/stage-timeline-chart.js
@@ -23,89 +23,33 @@ import JobChartFactories from "js/factories/job-chart-factories.js";
 import $ from "jquery";
 import stageTimeline from "../../../santosh/defination/stage-timeline";
 import pipelineTimeline from "../../../santosh/defination/pipeline-timeline";
-import { updateChartSize } from "../../../santosh/utils";
+import {updateChartSize} from "../../../santosh/utils";
 import * as echarts from "echarts";
 
 import GraphManager from "../../../santosh/GraphManager";
 
 console.log("stage-timeline-chart.js start");
 
-function addPipelineNamesToSelect(data) {
-  const pipelineSelector = document.getElementById("pipeline");
-
-  data.forEach((pipeline) => {
-    const selectOption = document.createElement("option");
-    selectOption.setAttribute("value", pipeline.name);
-    selectOption.text = pipeline.name;
-
-    pipelineSelector.appendChild(selectOption);
-  });
-}
-
-function requestPipelineList(transport) {
-  transport
-    .request("fetch-analytics", {
-      metric: "pipeline_list",
-    })
-    .done((data) => {
-      console.log("fetch-analytics ", data);
-      // this.initSeries(this.child.getNextGraphName(), JSON.parse(data));
-      addPipelineNamesToSelect(JSON.parse(data));
-      return JSON.parse(data);
-    })
-    .fail(console.error.toString());
-}
-
 AnalyticsEndpoint.onInit(function (initialData, transport) {
-  console.log("onInit called with initial data as ", initialData);
+    console.log("onInit called with initial data as ", initialData);
 
-  const data = JSON.parse(initialData);
+    const data = JSON.parse(initialData);
 
-  requestPipelineList(transport);
+    requestPipelineList(transport);
 
-  // factory = PipelineChartFactories.get("LongestWaitingPipelines"),
-  // container = document.getElementById("chart-container"),
-  // drilldowns = ["LongestWaitingJobs", "JobBuildTime"];
+    var chartDom = document.getElementById("chart-container");
 
-  // console.log("#1 Santosh data = ", data);
+    var myChart = echarts.init(chartDom);
+    updateChartSize(myChart, 1, 0.8);
 
-  // const config = DrilldownSupport.withDrilldown(
-  //     factory.config(data),
-  //     drilldowns,
-  //     JobChartFactories,
-  //     transport
-  // );
+    var option;
 
-  // console.log(config);
-
-  // H.chart(container, config);
-
-  // const point = {name: "up42"}
-  // const params = JobChartFactories.get("LongestWaitingJobs").params(point);
-
-  // dataBank.push(data);
-  // billboard_bar(transport);
-
-  // new StackedBar("LongestWaitingPipelines", data, drilldowns, transport);
-
-  // const graphManager = new GraphManager('series', transport);
-  // graphManager.initSeries('LongestWaitingPipelines', data);
-
-  // $("#chart-container").load("./pipeline-list.html");
-
-  var chartDom = document.getElementById("chart-container");
-
-  var myChart = echarts.init(chartDom);
-  updateChartSize(myChart, 1, 0.8);
-
-  var option;
-
-  const chartMeta = document.getElementById("chart-container-meta");
-  chartMeta.innerHTML = `
+    const chartMeta = document.getElementById("chart-container-meta");
+    chartMeta.innerHTML = `
     <div style="position:relative;"><span style="font-size:18px"><b>Stage timeline across workflow</b></span>
 
     <input type="checkbox" id="weird" name="weird" value="weird">
-<label for="weird"> Weird proof</label>
+    <label for="weird"> Weird proof</label>
 
     <select id="pipeline" style="float:right">
 </select>
@@ -113,34 +57,60 @@ AnalyticsEndpoint.onInit(function (initialData, transport) {
 <hr>
     `;
 
-  const pipelineSelector = document.getElementById("pipeline");
+    const pipelineSelector = document.getElementById("pipeline");
 
-  let selectedPipeline = pipelineSelector.value;
-  pipelineSelector.addEventListener("change", function () {
-    selectedPipeline = pipelineSelector.value;
+    let selectedPipeline = pipelineSelector.value;
+    pipelineSelector.addEventListener("change", function () {
+        selectedPipeline = pipelineSelector.value;
 
-    transport
-      .request("fetch-analytics", {
-        metric: "stage_timeline",
-        pipeline_name: selectedPipeline,
-      })
-      .done((data) => {
-        console.log("fetch-analytics ", data);
-        // this.initSeries(this.child.getNextGraphName(), JSON.parse(data));
-        option = null;
-        option = stageTimeline(JSON.parse(data), myChart);
-        option && myChart.setOption(option, true);
-      })
-      .fail(console.error.toString());
-  });
+        transport
+            .request("fetch-analytics", {
+                metric: "stage_timeline",
+                pipeline_name: selectedPipeline,
+            })
+            .done((data) => {
+                console.log("fetch-analytics ", data);
+                // this.initSeries(this.child.getNextGraphName(), JSON.parse(data));
+                option = null;
+                option = stageTimeline(JSON.parse(data), myChart);
+                option && myChart.setOption(option, true);
+            })
+            .fail(console.error.toString());
+    });
 
-  // option = pipelineTimeline();
-  // option && myChart.setOption(option);
+    // option = pipelineTimeline();
+    // option && myChart.setOption(option);
 
-  // const graphManager = new GraphManager("standalone", null);
-  // graphManager.initStandalone("pipeline-timeline", data);
+    // const graphManager = new GraphManager("standalone", null);
+    // graphManager.initStandalone("pipeline-timeline", data);
 
-  console.log("*********** stage-timeline graph loaded");
+    console.log("*********** stage-timeline graph loaded");
 });
+
+function addPipelineNamesToSelect(data) {
+    const pipelineSelector = document.getElementById("pipeline");
+
+    data.forEach((pipeline) => {
+        const selectOption = document.createElement("option");
+        selectOption.setAttribute("value", pipeline.name);
+        selectOption.text = pipeline.name;
+
+        pipelineSelector.appendChild(selectOption);
+    });
+}
+
+function requestPipelineList(transport) {
+    transport
+        .request("fetch-analytics", {
+            metric: "pipeline_list",
+        })
+        .done((data) => {
+            console.log("fetch-analytics ", data);
+            // this.initSeries(this.child.getNextGraphName(), JSON.parse(data));
+            addPipelineNamesToSelect(JSON.parse(data));
+            return JSON.parse(data);
+        })
+        .fail(console.error.toString());
+}
 
 AnalyticsEndpoint.ensure("v1");
