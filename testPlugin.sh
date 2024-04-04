d79d3409681272e20a26393b282914182d1216dd
Santosh Bachar <santoshbachar@gmail.com>
2024-04-04 21:46:59 +0530

    Clean

--- testPlugin.sh
+++ testPlugin.sh
@@ -7,6 +7,8 @@
 cd ./build/libs
 
 file=`ls -t ./gocd-analytics-plugin*.jar | head -n1`
+rm /Users/santoshbachar/dev/gocd/server/plugins/external/gocd-analytics-plugin*.jar
+
 cp $file /Users/santoshbachar/dev/gocd/server/plugins/external
 
 echo "Ready for testing. Reload the page."
