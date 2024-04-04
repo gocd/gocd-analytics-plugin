664010e56a08db19ad25f444ae1c40bbc2425024
Santosh Bachar <santoshbachar@gmail.com>
2024-04-04 21:42:29 +0530

    Clean

--- assets/js/pages/analytics-tab/global/agent-with-highest-utilization-chart.js
+++ assets/js/pages/analytics-tab/global/agent-with-highest-utilization-chart.js
@@ -17,38 +17,18 @@
 import "css/global";
 
 import AnalyticsEndpoint from "gocd-server-comms";
-import DrilldownSupport from "js/lib/drilldown-support.js";
-import AgentChartFactories from "js/factories/agent-chart-factories.js";
-import JobChartFactories from "js/factories/job-chart-factories.js";
-import H from "js/lib/load-highcharts.js";
-import StackedBar from "../../../santosh/stacked-bar";
 import GraphManager from "../../../santosh/GraphManager";
+import Console from "../../../santosh/Console";
+
+const c = new Console('agents-with-highest-utilization-chart.js', 'dev');
 
 AnalyticsEndpoint.onInit(function (initialData, transport) {
-    const agents = JSON.parse(initialData),
-        factory = AgentChartFactories.get("AgentsMostUtilized"),
-        drilldowns = ["LongestWaitingJobsOnAgent", "JobBuildTimeOnAgent"],
-        container = document.getElementById("chart-container");
+    const agents = JSON.parse(initialData);
 
-    const config = DrilldownSupport.withDrilldown(
-        factory.config(agents),
-        drilldowns,
-        JobChartFactories,
-        transport
-    );
-
-    // console.log("agents = ", agents);
-    // H.chart(container, config);
-
-    // const name = ['x', ...agents.map(a => a.agent_host_name)];
-    // const data1 = ['Idle Duration Secs', ...agents.map(a => a.idle_duration_secs)];
-    // new StackedBar(transport, drilldowns, name, data1, undefined, agents);
-
-    // new StackedBar("AgentsMostUtilized", agents, drilldowns, transport);
+    c.log("agents = " + agents);
 
     const graphManager = new GraphManager('series', transport);
     graphManager.initSeries('AgentsMostUtilized', agents);
-
 });
 
 AnalyticsEndpoint.ensure("v1");
