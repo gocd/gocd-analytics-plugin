133cdd50f30696bc0b7c85e113dc867df93b86c0
Santosh Bachar <santoshbachar@gmail.com>
2024-04-04 21:59:32 +0530

    Clean

--- assets/js/pages/analytics-tab/global/agent-with-highest-utilization-chart.js
+++ assets/js/pages/analytics-tab/global/agent-with-highest-utilization-chart.js
@@ -27,7 +27,7 @@ AnalyticsEndpoint.onInit(function (initialData, transport) {
 
     c.log("agents = " + agents);
 
-    const graphManager = new GraphManager('series', transport);
+    const graphManager = new GraphManager('series', transport, null, null, c);
     graphManager.initSeries('AgentsMostUtilized', agents);
 });
 
