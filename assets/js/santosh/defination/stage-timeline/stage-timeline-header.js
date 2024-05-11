af11b58a8182f563c14fdf835eb98d15d21c8891
Santosh Bachar <santoshbachar@gmail.com>
2024-05-11 19:24:44 +0530

    Add request order, limit

--- assets/js/santosh/defination/stage-timeline/stage-timeline-header.js
+++ assets/js/santosh/defination/stage-timeline/stage-timeline-header.js
@@ -3,6 +3,10 @@ import SlimSelect from 'slim-select';
 
 // const chartMeta = document.getElementById("chart-container-meta");
 let pipelineSelector = undefined;
+let dateSelector = undefined;
+let requestResultSelector = undefined;
+let requestOrderSelector = undefined;
+let requestLimitSelector = undefined;
 let resultFilterSelector = undefined;
 let dataFilterSelector = undefined;
 let legendVisibilityFilterSelector = undefined;
@@ -13,6 +17,9 @@ async function stageTimelineHeader(pipelines, settingsDOM) {
 
     await addOptionHeader(settingsDOM);
     pipelineSelector = document.getElementById("pipeline");
+    requestResultSelector = document.getElementById("requestResult");
+    requestOrderSelector = document.getElementById("requestOrder");
+    requestLimitSelector = document.getElementById("requestLimit");
     resultFilterSelector = document.getElementById("resultFilter");
     dataFilterSelector = document.getElementById("dataFilter");
     legendVisibilityFilterSelector = document.getElementById("legendVisibilityFilter");
@@ -23,22 +30,47 @@ async function stageTimelineHeader(pipelines, settingsDOM) {
         select: '#pipeline'
     })
 
-    await addOptionsToSelect(resultFilterSelector, ["Only Passed", "Only Failed", "Show both"]);
+    await addOptionsToSelect(requestResultSelector, [{text: "Passed", value: "Passed"}, {
+        text: "Failed",
+        value: "Failed"
+    }, {text: "Cancelled", value: "Cancelled"}, {text: "Any", valuer: "Any"}]);
+
+    await addOptionsToSelect(requestOrderSelector, [{text: "Ascending", value: "ASC"}, {
+        text: "Descending",
+        value: "DESC"
+    }]);
+
+    await addOptionsToSelect(resultFilterSelector, [{text: "Only Passed", value: "Only Passed"}, {
+        text: "Only Failed",
+        value: "Only Failed"
+    }, {text: "Only Cancelled", value: "Only Cancelled"}, {text: "Show both", value: "Show both"}]);
 
     // new SlimSelect({
     //     select: '#resultFilter'
     // })
 
-    await addOptionsToSelect(dataFilterSelector, ["time_waiting_secs", "time_building_secs", "total_time_secs"]);
+    await addOptionsToSelect(dataFilterSelector, [{
+        text: "Waiting time",
+        value: "time_waiting_secs"
+    }, {text: "Building time", value: "time_building_secs"}, {text: "Total time", value: "total_time_secs"}]);
 
     // new SlimSelect({
     //     select: '#dataFilter'
     // })
 
-    await addOptionsToSelect(legendVisibilityFilterSelector, ["Show", "Hide"]);
+    await addOptionsToSelect(legendVisibilityFilterSelector, [{text: "Show", value: "Show"}, {
+        text: "Hide",
+        value: "Hide"
+    }]);
 
 
-    return {pipelineSelector, resultFilterSelector, dataFilterSelector, legendVisibilityFilterSelector};
+    return {
+        pipelineSelector,
+        requestResultSelector,
+        resultFilterSelector,
+        dataFilterSelector,
+        legendVisibilityFilterSelector
+    };
 }
 
 async function addOptionHeader(settingsDOM) {
@@ -49,12 +81,24 @@ async function addOptionHeader(settingsDOM) {
     
     <div style="font-size:18px; flex-grow: 1"><b>Settings</b></div>
 
-    <div style="flex-grow: 1">
-    <input type="checkbox" id="weird" name="weird" value="weird">
-    <label for="weird"> Show only if data have delta</label>
-    </div>
+<!--    <div style="flex-grow: 1">-->
+<!--    <input type="checkbox" id="weird" name="weird" value="weird">-->
+<!--    <label for="weird"> Show only if data have delta</label>-->
+<!--    </div>-->
+    Request ->
+    <div style="display: flex; flex-direction: row; flex-grow: 1">
+    Result: <select id="requestResult"></select>
+</div>
 
-<div style="display: flex; flex-direction: row; flex-grow: 8">
+<div style="display: flex; flex-direction: row; flex-grow: 1">
+    Order: <select id="requestOrder"></select>
+</div>
+
+<div style="display: flex; flex-direction: row; flex-grow: 1">
+    Limit: <input id="requestLimit" type="number" min="1" value="10"/>
+</div>
+
+<div style="display: flex; flex-direction: row; flex-grow: 7">
     Pipeline: <select id="pipeline"></select>
 </div>
 
@@ -64,7 +108,7 @@ async function addOptionHeader(settingsDOM) {
 
 
 <div>
-Filter: <select id="resultFilter">
+Filter result: <select id="resultFilter">
 </select>
 </div>
 
