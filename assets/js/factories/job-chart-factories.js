b0720d9445a50684cf7a8c1eb530431cd92d3806
Santosh Bachar <santoshbachar@gmail.com>
2024-05-30 14:09:04 +0530

    Add StageRerunsInstances and StageStartupTimeCompare

--- assets/js/factories/job-chart-factories.js
+++ assets/js/factories/job-chart-factories.js
@@ -19,155 +19,188 @@ import JobCharts from "js/charts/jobs";
 import moment from "js/lib/moment-humanize-for-gocd.js";
 import GoCDLinkSupport from "js/lib/gocd-link-support";
 
-const TODAY        = moment(new Date()).format("YYYY-MM-DD"),
-      ONE_WEEK_AGO = moment(TODAY).add(-1, "week").format("YYYY-MM-DD");
+const TODAY = moment(new Date()).format("YYYY-MM-DD"),
+    ONE_WEEK_AGO = moment(TODAY).add(-1, "week").format("YYYY-MM-DD");
 
 export default new ChartFactories({
-  JobBuildTime: {
-    id: "job_build_time",
+    JobBuildTime: {
+        id: "job_build_time",
 
-    config: (data, transport) => {
-      const config = JobCharts.runs(data);
-      GoCDLinkSupport.linkToJobDetailsPage(config, transport);
-      return config;
+        config: (data, transport) => {
+            const config = JobCharts.runs(data);
+            GoCDLinkSupport.linkToJobDetailsPage(config, transport);
+            return config;
+        },
+
+        params: function (point) {
+            console.log('point is ', point);
+            return {
+                "job_name": point.job_name,
+                "stage_name": point.stage_name,
+                "pipeline_name": point.pipeline_name,
+                "type": "job",
+                "metric": this.id
+            };
+        }
     },
 
-    params: function (point) {
-      console.log('point is ', point);
-      return {
-        "job_name":      point.job_name,
-        "stage_name":    point.stage_name,
-        "pipeline_name": point.pipeline_name,
-        "type":          "job",
-        "metric":        this.id
-      };
+    JobBuildTimeOnAgent: {
+        id: "job_build_time_on_an_agent",
+
+        config: (data, transport) => {
+            const config = JobCharts.runsOnAnAgent(data);
+            GoCDLinkSupport.linkToJobDetailsPage(config, transport);
+            return config;
+        },
+
+        params: function (point) {
+            console.log('point is ', point);
+            return {
+                "type": "drilldown",
+                "metric": this.id,
+                "job_name": point.job_name,
+                "stage_name": point.stage_name,
+                "pipeline_name": point.pipeline_name,
+                "agent_uuid": point.agent_uuid,
+                "agent_host_name": point.agent_host_name
+            };
+        }
+    },
+
+    LongestWaitingJobs: {
+        id: "jobs_with_the_highest_wait_time",
+
+        config: (data) => {
+            return JobCharts.longestWaiting(data);
+        },
+
+        params: function (point) {
+            console.log('point is ', point);
+            return {
+                "pipeline_name": point.name,
+                "start": ONE_WEEK_AGO,
+                "end": TODAY,
+                "type": "job",
+                "metric": this.id
+            };
+        }
+    },
+
+    LongestWaitingJobsOnAgent: {
+        id: "jobs_with_the_highest_wait_time_on_an_agent",
+
+        config: (data) => {
+            return JobCharts.longestWaitingForAnAgent(data);
+        },
+
+        params: function (point) {
+            console.log('point is ', point);
+            return {
+                "type": "drilldown",
+                "metric": this.id,
+                "start": ONE_WEEK_AGO,
+                "end": TODAY,
+                "agent_uuid": point.agent_uuid,
+                "agent_host_name": point.agent_host_name
+            };
+        }
+    },
+
+    JobsTimeline: {
+        id: "job_timeline",
+
+        params: function (point) {
+            return {
+                "type": "drilldown",
+                "metric": this.id,
+                "stage_name": point.stage_name,
+                "pipeline_counter_start": point.pipeline_counter_start,
+                "pipeline_counter_end": point.pipeline_counter_end,
+            }
+        },
+    },
+
+    PipelinePriority: {
+        id: "priority_pipeline",
+
+        params: function (point) {
+            return {
+                "type": "drilldown",
+                "metric": this.id,
+                "result": point.result,
+            }
+        },
+    },
+
+    PipelinePriorityDetails: {
+        id: "priority_pipeline_details",
+
+        params: function (point) {
+            return {
+                "type": "drilldown",
+                "metric": this.id,
+                "pipeline_name": point.pipeline_name,
+                "result": point.result
+            }
+        },
+    },
+
+    StagePriorityDetails: {
+        id: "priority_stage_details",
+
+        params: function (point) {
+            return {
+                "type": "drilldown",
+                "metric": this.id,
+                "stage_name": point.stage_name,
+                "result": point.result
+            }
+        },
+    },
+
+    JobPriorityDetails: {
+        id: "priority_job_details",
+
+        params: function (point) {
+            return {
+                "type": "drilldown",
+                "metric": this.id,
+                "job_name": point.job_name,
+                "result": point.result
+            }
+        },
+    },
+
+    StageRerunsInstances: {
+        id: "stage_reruns",
+
+        params: function (point) {
+            let ret = {
+                "type": "drilldown",
+                "metric": this.id,
+                "pipeline_name": point.pipeline_name
+            }
+
+            if (point.hasOwnProperty("pipeline_counter")) {
+                ret.stage_name = point.stage_name;
+                ret.pipeline_counter = point.pipeline_counter;
+            }
+
+            return ret;
+        },
+    },
+
+    StageStartupTimeCompare: {
+        id: "stage_startup_time",
+
+        params: function (point) {
+            return {
+                "type": "drilldown",
+                "metric": this.id,
+                "pipeline_name": point.pipeline_name,
+                "pipeline_counter": point.pipeline_counter
+            }
+        },
     }
-  },
 
-  JobBuildTimeOnAgent: {
-    id: "job_build_time_on_an_agent",
-
-    config: (data, transport) => {
-      const config = JobCharts.runsOnAnAgent(data);
-      GoCDLinkSupport.linkToJobDetailsPage(config, transport);
-      return config;
-    },
-
-    params: function (point) {
-      console.log('point is ', point);
-      return {
-        "type":            "drilldown",
-        "metric":          this.id,
-        "job_name":        point.job_name,
-        "stage_name":      point.stage_name,
-        "pipeline_name":   point.pipeline_name,
-        "agent_uuid":      point.agent_uuid,
-        "agent_host_name": point.agent_host_name
-      };
-    }
-  },
-
-  LongestWaitingJobs: {
-    id: "jobs_with_the_highest_wait_time",
-
-    config: (data) => {
-      return JobCharts.longestWaiting(data);
-    },
-
-    params: function (point) {
-      console.log('point is ', point);
-      return {
-        "pipeline_name": point.name,
-        "start":         ONE_WEEK_AGO,
-        "end":           TODAY,
-        "type":          "job",
-        "metric":        this.id
-      };
-    }
-  },
-
-  LongestWaitingJobsOnAgent: {
-    id: "jobs_with_the_highest_wait_time_on_an_agent",
-
-    config: (data) => {
-      return JobCharts.longestWaitingForAnAgent(data);
-    },
-
-    params: function (point) {
-      console.log('point is ', point);
-      return {
-        "type":            "drilldown",
-        "metric":          this.id,
-        "start":           ONE_WEEK_AGO,
-        "end":             TODAY,
-        "agent_uuid":      point.agent_uuid,
-        "agent_host_name": point.agent_host_name
-      };
-    }
-  },
-
-  JobsTimeline: {
-    id: "job_timeline",
-
-    params: function (point) {
-      return {
-        "type": "drilldown",
-        "metric": this.id,
-        "stage_name": point.stage_name,
-        "pipeline_counter_start": point.pipeline_counter_start,
-        "pipeline_counter_end": point.pipeline_counter_end,
-      }
-    },
-  },
-
-  PipelinePriority: {
-    id: "priority_pipeline",
-
-    params: function (point) {
-      return {
-        "type": "drilldown",
-        "metric": this.id,
-        "result": point.result,
-      }
-    },
-  },
-
-  PipelinePriorityDetails: {
-    id: "priority_pipeline_details",
-
-    params: function (point) {
-      return {
-        "type": "drilldown",
-        "metric": this.id,
-        "pipeline_name": point.pipeline_name,
-        "result": point.result
-      }
-    },
-  },
-
-  StagePriorityDetails: {
-    id: "priority_stage_details",
-
-    params: function (point) {
-      return {
-        "type": "drilldown",
-        "metric": this.id,
-        "stage_name": point.stage_name,
-        "result": point.result
-      }
-    },
-  },
-
-  JobPriorityDetails: {
-    id: "priority_job_details",
-
-    params: function (point) {
-      return {
-        "type": "drilldown",
-        "metric": this.id,
-        "job_name": point.job_name,
-        "result": point.result
-      }
-    },
-  }
 });
