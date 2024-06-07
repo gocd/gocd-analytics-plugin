6110d233294000b945de626f3d29ccb4fc55e670
Santosh Bachar <santoshbachar@gmail.com>
2024-06-07 11:21:40 +0530

    Enhance to accommodate label truncate functionality

--- assets/js/santosh/defination/longest-waiting-pipeline.js
+++ assets/js/santosh/defination/longest-waiting-pipeline.js
@@ -1,6 +1,6 @@
 import GET_STACKED_BAR_TEMPLATE from "./stacked-bar";
 import {getBarSeries} from "../template";
-import {secondsToHms} from "../utils";
+import {getLabelTruncateAwareTooltipFormatterFunction} from "../TooltipHelper";
 
 /**
  * @class
@@ -9,7 +9,13 @@ import {secondsToHms} from "../utils";
 class LongestWaitingPipeline {
 
     data = null;
-    c= null;
+    c = null;
+
+    constructor(settings) {
+        this.settings = settings;
+
+        console.log('settings = ', settings);
+    }
 
     draw(data, c) {
 
@@ -21,8 +27,7 @@ class LongestWaitingPipeline {
         const option = GET_STACKED_BAR_TEMPLATE(info.categories, info.series);
         option.title.text = 'Pipelines with the Highest Wait Time';
 
-        // option.tooltip.formatter = this.tooltipFormatter();
-        option.tooltip.valueFormatter = (value) => secondsToHms(value);
+        option.tooltip.formatter = getLabelTruncateAwareTooltipFormatterFunction(info.actualCategories);
 
         return option;
     }
@@ -48,17 +53,42 @@ class LongestWaitingPipeline {
     }
 
     prepareData(data) {
+        const actualCategories = [];
         const categories = [];
         const data1 = [];
         const data2 = [];
 
+        function truncateString(text, truncateOrder, maxLength = 10) {
+            if (truncateOrder === 'None' || text.length <= maxLength) {
+                return text;
+            } else if (truncateOrder === 'Last') {
+                return text.substring(0, maxLength - 3) + "...";
+            } else if (truncateOrder === 'Middle') {
+                let numFirstChars = 2;
+                let numLastChars = 2;
+
+                if (text.length <= (numFirstChars + numLastChars + 3)) {
+                    return text; // String is already short enough
+                }
+                const middleLength = text.length - numFirstChars - numLastChars;
+                if (middleLength <= 3) {
+                    // Not enough space for ellipsis, shorten last characters
+                    numLastChars = Math.max(0, middleLength);
+                }
+                return `${text.slice(0, numFirstChars)}...${text.slice(-numLastChars)}`;
+            }
+
+        }
+
         data.forEach(m => {
-            categories.push(m.name);
+            actualCategories.push(m.name);
+            categories.push(truncateString(m.name, this.settings.truncateOrder));
             data1.push(m.avg_wait_time_secs);
             data2.push(m.avg_build_time_secs);
         });
 
         return {
+            actualCategories: actualCategories,
             categories: categories,
             series: [getBarSeries('Wait Time', data1), getBarSeries('Build Time', data2)],
             // tooltip: getTooltip
