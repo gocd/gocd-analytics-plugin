64da225ddf83e80d9e8e8aa139e720cb49338586
Santosh Bachar <santoshbachar@gmail.com>
2025-03-29 15:50:24 +0530

    Add agent results

---
+++ assets/js/pages/analytics-tab/global/agent-with-lowest-utilization-chart.js
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2020 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import "css/global";
+
+import AnalyticsEndpoint from "gocd-server-comms";
+import GraphManager from "../../../santosh/GraphManager";
+import Console from "../../../santosh/Console";
+import RequestMaster from "../../../RequestMaster";
+import Header from "../../../santosh/defination/stage-timeline/header";
+import {getFirstDayOfTheCurrentMonth, getTodaysDateInDBFormat} from "../../../santosh/utils";
+import Footer from "../../../santosh/defination/stage-timeline/footer";
+
+let graphManager = null;
+let requestMaster = null;
+
+let header = null;
+let footer = null;
+
+const c = new Console('agents-with-lowest-utilization-chart.js', 'dev');
+
+const settings = {
+    "AgentsLeastUtilized": {
+        truncateOrder: 'Last',
+        startDate: getFirstDayOfTheCurrentMonth(),
+        endDate: getTodaysDateInDBFormat(),
+        limit: 10
+    }
+    // , "LongestWaitingJobsOnAgent": {
+    //     truncateOrder: 'Last',
+    //     startDate: getFirstDayOfTheCurrentMonth(),
+    //     endDate: getTodaysDateInDBFormat(),
+    //     limit: 10
+    // }, "JobBuildTimeOnAgent": null
+};
+
+AnalyticsEndpoint.onInit(function (initialData, transport) {
+    const agents = JSON.parse(initialData);
+
+    c.log("agents = ", agents);
+
+    requestMaster = new RequestMaster(transport);
+    header = new Header(requestMaster);
+    footer = new Footer();
+
+    graphManager = new GraphManager('series', transport, informSeriesMovement, footer);
+
+    init(agents);
+
+});
+
+async function init(data) {
+    settings["AgentsLeastUtilized"] = await header.getLongestWaitingPipelinesHeader(graphOneHeaderChangeHandler);
+
+    if (data.length === 0) {
+        footer.showMessage("No data to display", "INFO", true);
+    } else {
+        graphManager.initSeries('AgentsLeastUtilized', data, settings['AgentsLeastUtilized']);
+    }
+}
+
+async function informSeriesMovement(graphName, requestParams) {
+    console.log('📞 I am informed of graphName ', graphName, ' changing the header now.');
+    console.log('requestParams', requestParams);
+    // return header.switchHeader(graphName);
+    header.clear();
+
+    let headerSettings = null;
+
+    switch (graphName) {
+        case "AgentsLeastUtilized":
+            headerSettings = await header.getLongestWaitingPipelinesHeader(graphOneHeaderChangeHandler, settings["AgentsMostUtilized"]);
+            break;
+
+        case "LongestWaitingJobsOnAgent":
+            // if(settings.LongestWaitingJobs === null) {
+            //     settings["LongestWaitingJobs"] = await header.getLongestWaitingPipelinesHeader(graphTwoHeaderChangeHandler);
+            //     headerSettings = settings["LongestWaitingJobs"];
+            // } else {
+            settings["LongestWaitingJobsOnAgent"]["startDate"] = settings["AgentsMostUtilized"]["startDate"];
+            settings["LongestWaitingJobsOnAgent"]["endDate"] = settings["AgentsMostUtilized"]["endDate"];
+
+            headerSettings = await header.getLongestWaitingPipelinesHeader(graphTwoHeaderChangeHandler, settings["LongestWaitingJobsOnAgent"]);
+            // }
+            break;
+
+        default:
+            return null;
+    }
+
+    return headerSettings;
+}
+
+async function graphOneHeaderChangeHandler(changedSettings) {
+    c.log("graphOneHeaderChangeHandler", changedSettings);
+
+    const previousTruncateOrder = settings["AgentsLeastUtilized"].truncateOrder;
+
+    settings["AgentsLeastUtilized"] = changedSettings;
+
+    if (previousTruncateOrder === changedSettings.truncateOrder) {
+        const agents = await requestMaster.getAgentLeastUtilized(changedSettings.startDate, changedSettings.endDate, changedSettings.limit);
+        graphManager.clear();
+        if (agents.length === 0) {
+            footer.showMessage("No data to display", "Error", true);
+        } else {
+            graphManager.call_initSeriesWithNewData(agents);
+            footer.clear();
+        }
+    } else {
+        graphManager.call_initSeriesWithNewSettings(changedSettings);
+    }
+}
+
+function graphTwoHeaderChangeHandler(changedSettings) {
+    c.log("graphTwoHeaderChangeHandler", changedSettings);
+
+    settings["LongestWaitingJobsOnAgent"] = changedSettings;
+    graphManager.call_initSeriesWithNewSettings(changedSettings);
+}
+
+AnalyticsEndpoint.ensure("v1");
---
+++ assets/js/santosh/defination/agents-least-utilized.js
@@ -0,0 +1,91 @@
+import GET_STACKED_BAR_TEMPLATE from "./stacked-bar";
+import {getBarSeries} from "../template";
+import {secondsToHms, truncateString} from "../utils";
+import {getLabelTruncateAwareTooltipFormatterFunction} from "../TooltipHelper";
+
+/**
+ * @class
+ * @interface {ChartInterface}
+ */
+class AgentsLeastUtilized {
+
+    data = null;
+    c = null;
+
+    constructor(settings) {
+        this.settings = settings;
+
+        console.log('settings = ', settings);
+    }
+
+    draw(data, c) {
+
+        this.data = data;
+        this.c = c;
+
+        const info = this.prepareData(this.data);
+
+        const option = GET_STACKED_BAR_TEMPLATE(info.categories, info.series);
+        option.title.text = 'Agents with the Least Utilization';
+        // option.tooltip.valueFormatter = (value) => secondsToHms(value);
+
+        // option.series.label = {
+        //     show: true,
+        //     position: 'top',
+        //     formatter: '{b}'
+        // }
+        //
+        // option.yAxis.axisLabel = {
+        //     show: false,
+        // }
+
+        // option.tooltip.formatter = this.tooltipFormatter();
+
+        option.tooltip.formatter = getLabelTruncateAwareTooltipFormatterFunction(info.actualCategories);
+
+        return option;
+    }
+
+    prepareData(data) {
+        const actualCategories = [];
+        const categories = [];
+        const data1 = [];
+
+        data.forEach(m => {
+            actualCategories.push(m.agent_host_name);
+            categories.push(truncateString(m.agent_host_name, this.settings.truncateOrder, 15));
+            data1.push(m.idle_duration_secs);
+        });
+
+        return {
+            actualCategories: actualCategories,
+            categories: categories,
+            series: getBarSeries('Agents', data1)
+        }
+    }
+
+
+    get_requestParamsPoint(index) {
+        return {
+            "agent_uuid": this.data[index].uuid, "agent_host_name": this.data[index].agent_host_name,
+        }
+    }
+
+    getNextGraphName() {
+        return "LongestWaitingJobsOnAgent";
+    }
+
+    insertBreadcrumb() {
+        return false;
+    }
+
+    breadcrumbCaption() {
+        return "Agents";
+    }
+
+    getSeriesIndex() {
+        return 0;
+    }
+}
+
+export default AgentsLeastUtilized;
