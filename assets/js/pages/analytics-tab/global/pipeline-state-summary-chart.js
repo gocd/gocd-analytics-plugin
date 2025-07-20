79be9b7067b0b1034b2eaaf3721f26128f22361e
Santosh Bachar <santoshbachar@gmail.com>
2025-07-20 15:12:35 +0530

    Add date header support

--- assets/js/pages/analytics-tab/global/pipeline-state-summary-chart.js
+++ assets/js/pages/analytics-tab/global/pipeline-state-summary-chart.js
@@ -19,16 +19,70 @@ import "css/global";
 import AnalyticsEndpoint from "gocd-server-comms";
 import GraphManager from "../../../santosh/GraphManager";
 import Console from "../../../santosh/Console";
+import RequestMaster from "../../../RequestMaster";
+import Header from "../../../santosh/defination/stage-timeline/header";
+import Footer from "../../../santosh/defination/stage-timeline/footer";
+import {
+  getFirstDayOfTheCurrentMonth,
+  getTodaysDateInDBFormat
+} from "../../../santosh/utils";
 
-const c = new Console('pipeline-state-summary-chart.js');
+let graphManager = null;
+let requestMaster = null;
+
+let header = null;
+let footer = null;
+
+const c = new Console('pipeline-state-summary-chart.js', 'dev');
+
+let settings = {
+  startDate: getFirstDayOfTheCurrentMonth(),
+  endDate: getTodaysDateInDBFormat(),
+};
 
 AnalyticsEndpoint.onInit(function (initialData, transport) {
   const data = JSON.parse(initialData);
 
-  const graphManager = new GraphManager("standalone", null, null, null);
-  graphManager.initStandalone("pipeline-state-summary", data);
+  requestMaster = new RequestMaster(transport);
+  header = new Header(requestMaster);
+  footer = new Footer();
+
+  graphManager = new GraphManager("standalone", transport, null,
+      footer);
+
+  init(data);
 
   console.log("*********** pipeline-state-summary graph loaded");
 });
+
+async function init(data) {
+  settings = await header.getPipelineStateSummaryHeader(
+      graphOneHeaderChangeHandler, settings);
+
+  if (data.length === 0) {
+    footer.showMessage("No data to display", "INFO", true);
+  } else {
+    graphManager.initStandalone("pipeline-state-summary", data);
+  }
+
+}
+
+async function graphOneHeaderChangeHandler(changedSettings) {
+  c.logs('📞 I am informed of changedSettings ', changedSettings);
+
+  console.log("📞 I am informed of changedSettings", changedSettings);
+
+  const pipelines = await requestMaster.getPipelineStateSummary(changedSettings);
+
+  graphManager.clear();
+
+  if (pipelines.length === 0) {
+    footer.showMessage("No data to display", "Error", true);
+  } else {
+    graphManager.call_initStandaloneWithNewData(pipelines);
+    footer.clear()
+  }
+
+}
 
 AnalyticsEndpoint.ensure("v1");
